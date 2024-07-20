e6eb5a921aa30878ae5b5876e2d8efd7c9bf1b7e
Shakil Ahmed <ahmed1shakil@gmail.com>
2024-07-20 14:14:58 +0100

    added the error messages and redirection

---
+++ validation.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('#form');
+    const userEmailInput = document.querySelector('#email-input');
+    const errorDiv = document.querySelector('#email-error');
+    const emailPatternRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  
+    form.addEventListener('submit', (event) => {
+      event.preventDefault(); // Prevent form submission
+      
+      let error = '';
+      const emailValue = userEmailInput.value.trim();
+  
+      if (!emailValue) {
+        error = 'Please provide an email';
+      } else if (!emailPatternRegex.test(emailValue)) {
+        error = 'Valid email required';
+      }
+  
+      if (error) {
+        errorDiv.textContent = error;
+        errorDiv.style.color = 'orange';
+        userEmailInput.style.borderColor = 'orange';
+      } else {
+        window.location.href = 'success-page.html';
+      }
+    });
+  });
+  
